6e8eddc5c8a445855df6316af6db0be554052f6e
kharann <anhkhav@gmail.com>
2019-04-12 18:48:01 +0200

    removed unecesarry div; removed underline class because i can just use margin;

--- splash/src/core/components/MenuTab.js
+++ splash/src/core/components/MenuTab.js
@@ -2,15 +2,13 @@ import React from 'react';
 import style from './MenuTab.module.css'
 import classNames from 'classnames';
 
-const MenuTab = ({isActive, children, handleOnclick}) => {
+const MenuTab = ({ isActive, children, handleOnclick }) => {
     return (
         <div className={classNames(
             isActive ? style.isActive : style.notActive,
             style.menu
-            )}>
-            <div className={classNames(style.underline)}>
-               {children} 
-            </div>
+        )}>
+            {children}
         </div>
     );
 };
