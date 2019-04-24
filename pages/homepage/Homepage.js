08a38dfb03e49b8e95c05dad7cef507757e0dd3d
blauks <brooks.jonathan1@gmail.com>
2019-04-24 23:19:57 +0200

    Styled homepage

---
+++ pages/homepage/Homepage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Circle from './Circle'
+import Polygon1 from './Polygon1';
+import Polygon2 from './Polygon2';
+import styled from 'styled-components';
+
+const PolyOne = styled(Polygon1)`
+    
+    position: fixed;
+    right: 0px;
+    top: -2.6px;
+
+`;
+
+const PolyTwo = styled(Polygon2)`
+
+    position: fixed;
+    left: 2px;
+    bottom: 1px;
+
+`;
+
+const StyledCircle = styled(Circle)`
+
+    position: fixed;
+    top: 128px;
+    left: 269px;
+
+`
+
+const Homepage = () => (
+  <div className="Frontpage">
+        <StyledCircle/>
+
+        <PolyOne/>
+
+        <PolyTwo/>
+  </div>
+  );
+
+export default Homepage;
