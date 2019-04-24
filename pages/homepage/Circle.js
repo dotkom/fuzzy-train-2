c5af0b4a62c207e928b2f73e3b8a714646ec4fdb
kharann <anhkhav@gmail.com>
2019-04-24 23:23:27 +0200

    removed testing code

--- pages/homepage/Circle.js
+++ pages/homepage/Circle.js
@@ -2,13 +2,12 @@ import React, {useState} from 'react';
 import styled from 'styled-components'
 
 
-const Circle = ({className}) => {
+const Circle = ({className, colorScheme }) => {
   const [colorscheme, switchColorscheme] = useState("white");
 
   return ( 
-    <svg className={className} width="217" height="245" viewBox="0 0 217 245" fill="none" xmlns="http://www.w3.org/2000/svg">
-     <circle cx="126" cy="91" r="91" fill={colorscheme} 
-     onClick={() => colorscheme == "white" ? switchColorscheme("#07244C") : switchColorscheme("white")}
+    <svg  width="217" height="245" viewBox="0 0 217 245" fill="none" xmlns="http://www.w3.org/2000/svg">
+     <circle className={className} cx="126" cy="91" r="91" fill={colorscheme} 
      />
      <circle cx="63" cy="182" r="63" fill="#DC942D"/>
     </svg>
