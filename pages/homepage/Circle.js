e309ccabb4fb9511c916ccc3977e42c311b0564d
kharann <anhkhav@gmail.com>
2019-04-24 21:59:05 +0200

    Created double circle component and tested switchTheme

--- pages/homepage/Circle.js
+++ pages/homepage/Circle.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Circle = ({switchTheme}) => {
+const Circle = ({preferedTheme}) => {
+  const [colorscheme, switchTheme] = useState("white");
+
   return ( 
     <svg width="217" height="245" viewBox="0 0 217 245" fill="none" xmlns="http://www.w3.org/2000/svg">
-     <circle cx="126" cy="91" r="91" fill="white" onclick={switchTheme}/>
+     <circle cx="126" cy="91" r="91" fill={colorscheme} 
+     onClick={() => colorscheme == "white" ? switchTheme("#07244C") : switchTheme("white")}
+     />
      <circle cx="63" cy="182" r="63" fill="#DC942D"/>
     </svg>
   );
