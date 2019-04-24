9bf8ec7692fa4878554b9476d464170554fa6ed0
blauks <brooks.jonathan1@gmail.com>
2019-04-24 23:19:57 +0200

    Styled homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import Homepage from './homepage/homepage'
+import Homepage from './homepage/Homepage';
+import styled,{ createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+:root {
+    --dark: #07244C;
+}
+    body{
+        background-color: var(--dark);
+    }
+
+`
 
 const Index = () => (
+    <>
+    <GlobalStyle/>
     <Homepage/>
+    </>
 );
 
 export default Index;
