aaaf3cf97c60bb3e2cf097c1a62a7b838c646c8c
kharann <anhkhav@gmail.com>
2019-04-24 22:55:01 +0200

    fix styled components functionality with nextjs

--- pages/homepage/Circle.js
+++ pages/homepage/Circle.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
+import styled from 'styled-components'
 
-const Circle = ({preferedTheme}) => {
-  const [colorscheme, switchTheme] = useState("white");
+
+const Circle = ({className}) => {
+  const [colorscheme, switchColorscheme] = useState("white");
 
   return ( 
-    <svg width="217" height="245" viewBox="0 0 217 245" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg className={className} width="217" height="245" viewBox="0 0 217 245" fill="none" xmlns="http://www.w3.org/2000/svg">
      <circle cx="126" cy="91" r="91" fill={colorscheme} 
-     onClick={() => colorscheme == "white" ? switchTheme("#07244C") : switchTheme("white")}
+     onClick={() => colorscheme == "white" ? switchColorscheme("#07244C") : switchColorscheme("white")}
      />
      <circle cx="63" cy="182" r="63" fill="#DC942D"/>
     </svg>
