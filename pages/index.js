884fc0cd627a23d2ff71a2711445c3855ce58200
kharann <anhkhav@gmail.com>
2019-05-01 21:39:08 +0200

    fix path name

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Homepage from './homepage/Homepage';
 import GlobalStyle from './components/GlobalStyle'
-import { ThemeProvider } from './components/themeComponents/ThemeProvider';
+import { ThemeProvider } from './components/ThemeComponents/ThemeProvider';
 
 const Index = () => {
   return (
