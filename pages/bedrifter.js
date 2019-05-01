f71bb71fb685f88f45e211b13e16dd607dbd8ff7
kharann <anhkhav@gmail.com>
2019-05-01 21:54:30 +0200

    prettier stuff

--- pages/bedrifter.js
+++ pages/bedrifter.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import GlobalStyle from './components/GlobalStyle'
-import Menu from './components/MenuComponents/MainMenu'
+import GlobalStyle from './components/GlobalStyle';
+import Menu from './components/MenuComponents/MainMenu';
 import { ThemeProvider } from './components/ThemeComponents/ThemeProvider';
 
 //Temporary file for å teste routing
 const Bedrifter = () => {
   const tabs = [
-    { text: "Generell Info", link: "/students", isActive: false },
-    { text: "Profilering", link: "/", isActive: true },
-    { text: "Faglig", link: "/bedrifter", isActive: false },
-    { text: "Meld Interesse", link: "/bedrifter", isActive: false },
-  ]
+    { text: 'Generell Info', link: '/students', isActive: false },
+    { text: 'Profilering', link: '/', isActive: true },
+    { text: 'Faglig', link: '/bedrifter', isActive: false },
+    { text: 'Meld Interesse', link: '/bedrifter', isActive: false },
+  ];
 
   return (
     <ThemeProvider>
       <GlobalStyle />
-      <Menu tabs={tabs}/>
+      <Menu tabs={tabs} />
     </ThemeProvider>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Homepage from './homepage/Homepage';
-import GlobalStyle from './components/GlobalStyle'
+import GlobalStyle from './components/GlobalStyle';
 import { ThemeProvider } from './components/ThemeComponents/ThemeProvider';
 
 const Index = () => {
