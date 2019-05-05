a65914b8128b22ec32a173a34e45fb67d7ec0f06
kharann <anhkhav@gmail.com>
2019-05-05 15:16:16 +0200

    Center the elements for company page

--- pages/companies/index.js
+++ pages/companies/index.js
@@ -10,27 +10,6 @@ import Footer from '../components/FooterComponents/Footer';
 import GenerellInfo from './GenerellInfo';
 import Tabs from './CompaniesTabs';
 
-const Menu = styled(MenuContainer)`
-  min-width: 60%;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  gap: 1rem;
-  align-items: auto;
-
-  & > a {
-    font-size: 14px;
-  }
-`;
-
-const TopWrapper = styled.div`
-  width: 60%;
-  font-size: 14px;
-`;
-const PageBody = styled.div`
-  width: 100%;
-  height: 100%;
-  padding: 2em;
-`;
 //Temporary file for å teste routing
 const Bedrifter = () => {
   return (
