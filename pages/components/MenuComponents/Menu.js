e61315754abb065671ac8ee965fc7eb612d0b1c6
kharann <anhkhav@gmail.com>
2019-05-26 14:51:56 +0200

    moved css to menu

--- pages/components/MenuComponents/Menu.js
+++ pages/components/MenuComponents/Menu.js
@@ -1,37 +1,14 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-import MenuTabContainer from './MenuTab';
-
-const MenuTab = styled(MenuTabContainer)`
-  font-weight: 900;
-  font-size: 1.5rem;
-  line-height: 2em;
-
-  @media only screen and (max-width: 768px) {
-    font-size: 1.5em;
-  }
-
-  ${props =>
-    props.underline &&
-    css`
-      border-bottom: ${props.isActive ? '3px' : '2px'} solid;
-      border-bottom-color: ${props => (props.isActive ? props.theme.orange : '#c4c4c4')};
-    `}
-`;
+import MenuTab from './MenuTab';
 
 const Menu = ({ className, tabs, activeTab, underline }) => {
   const MenuTabs = tabs.map((tab, index) => (
-    <MenuTab
-      text={tab.text}
-      link={tab.link}
-      isActive={index == activeTab}
-      key={index}
-      underline={underline}
-    />
+    <MenuTab text={tab.text} link={tab.link} isActive={index == activeTab} key={index} underline={underline} />
   ));
 
   return <div className={className}>{MenuTabs}</div>;
-}
+};
 
 export default Menu;
--- pages/components/MenuComponents/MenuTab.js
+++ pages/components/MenuComponents/MenuTab.js
@@ -3,9 +3,9 @@ import { css } from 'styled-components';
 import styled from 'styled-components';
 import Link from 'next/link';
 
-const MenuTabContainer = ({ className, link, text, browserUrl }) => {
+const MenuTabContainer = ({ className, link, text }) => {
   return (
-    <Link href={link} >
+    <Link href={link}>
       <a className={className}>{text}</a>
     </Link>
   );
@@ -16,5 +16,24 @@ const MenuTab = styled(MenuTabContainer)`
   text-align: center;
   color: ${props => props.theme.secondary};
   opacity: ${props => (props.isActive ? 1 : 0.7)};
+  font-weight: 900;
+  font-size: 1.5rem;
+  line-height: 2em;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 1.5em;
+  }
+
+  ${props =>
+    props.underline &&
+    css`
+      border-bottom: ${props.isActive ? '3px' : '2px'} solid;
+      border-bottom-color: ${props.isActive ? props.theme.orange : '#c4c4c4'};
+    `}
 `;
+
+const SubMenuTab = styled(MenuTabContainer)` 
+
+`
+
 export default MenuTab;
