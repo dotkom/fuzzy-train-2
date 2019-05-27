11aff22a582025c8008a6c9932fe397f93bd0ce0
kharann <anhkhav@gmail.com>
2019-05-27 16:54:27 +0200

    Moved components folder away from pages folder

--- pages/companies/index.js
+++ pages/companies/index.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
-import GlobalStyle from '../components/GlobalStyle';
-import { ThemeProvider, useTheme } from '../components/ThemeComponents/ThemeProvider';
-import Title from '../components/MenuComponents/Title';
-import Footer from '../components/FooterComponents/Footer';
 import Tabs from './Tabs';
 import GenerellInfo from './info';
-import PageBody from '../components/MenuComponents/PageBody';
+import PageBody from '../../components/MenuComponents/PageBody';
 
 //Temporary file for å teste routing
 const Bedrifter = () => {
