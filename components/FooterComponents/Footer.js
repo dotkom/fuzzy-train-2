57174c00b0e98330844fa6f392d8e7230514a332
kharann <anhkhav@gmail.com>
2019-05-29 18:11:17 +0200

    cute but failed try at creating hte footer

--- components/FooterComponents/Footer.js
+++ components/FooterComponents/Footer.js
@@ -1,47 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 import FooterSVG from './Footer.svg';
-import FooterDark from './FooterDark.svg';
-import FooterLight from './FooterLight.svg';
 import LogoSVG from '../MenuComponents/Logo.svg';
 import { useTheme, ThemeToggleProvider } from '../ThemeComponents/ThemeContext';
 
 const Logo = styled(LogoSVG)`
-  position: absolute;
-  right: 20px;
-  bottom: 20px;
   width: 40vmin;
   fill: var(--primary);
+  position: absolute;
+  bottom: 0;
+  right: 0;
 `;
 
 const Container = styled.div`
+display: flex;
   width: 100%;
-  height: 300px;
-`;
-
-const RightDiv = styled.div`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 100vmin;
-  height: 30vmin;
-  transform: rotate(20deg) translate(-5%, 60%);
-  background: #dc942d;
-`;
-
-const LeftDiv = styled.div`
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  width: 125%;
-  height: 25vmin;
-  transform: rotate(-5deg) translate(10%, 45%);
-  background: var(--secondary);
+  height: 100%;
+  fill: var(--secondary);
 `;
 
 const Footer = ({ theme }) => {
   const themeState = useTheme();
-  return <Container>{themeState.theme == 'light' ? <FooterDark /> : <FooterLight />}</Container>;
+  return (
+    <Container>
+      <svg width="1920" height="355" viewBox="0 0 1920 355" xmlns="http://www.w3.org/2000/svg">
+        <path d="M-117 0C-117 0 623 254.161 665 290.18C707 326.2 694.5 365.132 697.5 449H-117V0Z" fill="#DC942D" />
+        <rect x="1920" y="356" width="1920" height="54.9998" transform="rotate(-180 1920 356)" />
+        <path d="M1920 301.5L0 301.5L1920 143L1920 301.5Z" />
+      </svg>
+      <Logo />
+    </Container>
+  );
 };
 
 export default Footer;
